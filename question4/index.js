a951005939bea670e177bac0bf7ce4fe33937082
BilRos <billyrossowuor@gmail.com>
2024-05-06 22:31:56 +0300

    Palindrum

---
+++ question4/index.js
@@ -0,0 +1,18 @@
+function palindrome(x) {
+    // Convert the integer to a string
+    const str = x.toString();
+    
+    // Iterate through the string from both ends towards the center
+    for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+        // If characters at current positions don't match, return false
+        if (str[i] !== str[j]) {
+            return false;
+        }
+    }
+    // If the loop completes without returning false, it's a palindrome
+    return true;
+}
+
+// Test cases
+console.log(palindrome(121)); // true
+console.log(palindrome(-121)); // false
